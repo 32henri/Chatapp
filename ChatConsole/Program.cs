3e7d3040dc894409572e55465139c19e25913406
Opilane TTHK <opilane@tthk.ee>
2024-10-17 11:27:09 +0300

    77

---
+++ ChatConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿using ChatApp.Net.IO;
+using ChatConsole;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+
+
+
+Server server = new Server();
+server.ConnectToServer("name");
+server.SendMessageToServer("aaaa");
