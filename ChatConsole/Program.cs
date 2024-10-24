3285d783ac929cf23a01c38af1c8fec9a7a3f56a
Opilane TTHK <opilane@tthk.ee>
2024-10-24 11:26:28 +0300

    connect and disscont

--- ChatConsole/Program.cs
+++ ChatConsole/Program.cs
@@ -3,24 +3,71 @@ using ChatConsole;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = string.Empty;
+        string message;
+        string input;
 
+        DisplayCommands();
 
+        Server server = new Server();
 
-string name;
-string message;
+        while (true)
+        {
+            server.connectedEvent += () => Console.WriteLine("User " + name + " connected to the server");
+            server.userDisconnectedEvent += () => Console.WriteLine("User " + name + " disconnected to the server");
+            Console.Write("Enter command: ");
+            input = Console.ReadLine();
 
-Console.Write("Nimi: ");
-name = Console.ReadLine();
+            if (input.Equals("login", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
 
-Server server = new Server();
-server.ConnectToServer(name);
+                if (server.ConnectToServer(name))
+                {
+                    Console.WriteLine("User logged in as: " + name);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to log in to the server. Please try again.");
+                }
+            }
+            else if (input.Equals("message", StringComparison.OrdinalIgnoreCase) && !string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Message: ");
+                message = Console.ReadLine();
+                server.SendMessageToServer(message);
+                server.msgReceivedEvent += () => Console.WriteLine(name + " sent message " + message);
+                //Console.WriteLine($"{name} sent message: {message}");
+            }
+            else if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Exiting the application...");
+                break; 
+            }
+            else if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("You need to log in first by typing 'log in'.");
+            }
+            else
+            {
+                Console.WriteLine("Unknown command. Please try again.");
+            }
+        }
 
-Console.Write("Message: ");
-message = Console.ReadLine();
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
 
-server.SendMessageToServer(message);
-Console.WriteLine(name + " sent message: " + message);
-
-// If you want to keep the console open until the user presses a key
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+    static void DisplayCommands()
+    {
+        Console.WriteLine("Command list:");
+        Console.WriteLine("To log in, type 'login'.");
+        Console.WriteLine("To send a message, type 'message'.");
+        Console.WriteLine("To exit, type 'exit'.");
+    }
+}
