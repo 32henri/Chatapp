f4d0f78359053f4f1bcc7d66aee48a9360fb0d35
Opilane TTHK <opilane@tthk.ee>
2024-11-22 11:05:09 +0200

    manuaalselt tuleb lisada kasutajaid

--- ChatConsole/Program.cs
+++ ChatConsole/Program.cs
@@ -10,50 +10,30 @@ using ChatConsole.Model;
 
 class Program
 {
+
+
     static void Main()
     {
-        string name = Environment.GetEnvironmentVariable("USERNAME");
-
-        if (string.IsNullOrEmpty(name))
-        {
-            // If the environment variable is not set, ask the user to input the username
-            Console.Write("Enter your username: ");
-            name = Console.ReadLine();
-        }
-
-        if (string.IsNullOrEmpty(name))
-        {
-            Console.WriteLine("Username cannot be empty. Exiting...");
-            return;
-        }
-
-        var server = new Server();
-
-        // Attempt to connect to the server
-        if (!server.ConnectToServer(name))  // Connect with the dynamic username
-        {
-            Console.WriteLine("Connection failed. Ensure the server is running.");
-            return;
-        }
-
-        Console.WriteLine($"Connected as {name}. You can now start sending messages.");
-
+        Console.WriteLine("Enter name to connect");
+        var name = Console.ReadLine();
+        var message = string.Empty;
         var input = new MainViewModel(name);
 
-        // Allow the user to send messages
+
         while (true)
         {
-            Console.Write("Enter message: ");
-            var message = Console.ReadLine();
-            if (!string.IsNullOrEmpty(message))
+            message = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(message))
             {
                 input.SendMessage(message);
-                server.SendMessageToServer(message);
             }
             else
             {
-                Console.WriteLine("Please enter a message.");
+                Console.WriteLine("Message cannot be empty. Please try again.");
             }
         }
+
+
+
     }
 }
